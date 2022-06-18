9e075c41b1dc1f3d99290598a7b6f6f3842fa330
nathaliaclarissa <90769498+nathaliaclarissa@users.noreply.github.com>
2022-06-19 00:48:44 +0700

    Add files via upload

---
+++ Tugas 5/src/com/company/CarRider.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class CarRider {
+    public String name;
+    public int age;
+    public String phone;
+
+    public CarRider(String name, int age, String phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
